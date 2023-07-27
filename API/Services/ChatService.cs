d51574f22348ef781441ca35f15c77a02b7b5c88
Md. Wahid <mdwahid5048@gmail.com>
2023-07-27 16:03:40 +0600

    Innitial commit and chat service created

---
+++ API/Services/ChatService.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class ChatService
+    {
+        private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
+        public bool AddUserToList(string userToAdd)
+        {
+            lock(Users)
+            {
+                foreach (var user in Users)
+                {
+                    if(user.Key.ToLower()==userToAdd.ToLower()){
+                        return false;
+                    }
+                }
+
+                Users.Add(userToAdd, null);
+                return true;
+            }
+        }
+
+        public void AddUserConnectionId(string user, string connectionId)
+        {
+            lock(Users)
+            {
+                if(Users.ContainsKey(user)){
+                    Users[user] = connectionId;
+                }
+            }
+        }
+
+        public string GetConnectionIdByUser(string user)
+        {
+            lock(Users)
+            {
+                return Users.Where(x => x.Key==user).Select(x => x.Value).FirstOrDefault();
+            }
+        }
+
+        public string GetUserByConnectionId(string connectionId)
+        {
+            lock(Users)
+            {
+                return Users.Where(x => x.Value==connectionId).Select(x => x.Key).FirstOrDefault();
+            }
+        }
+
+        public void RemoveUserFromList(string user)
+        {
+            lock(Users)
+            {
+                if(Users.ContainsKey(user))
+                {
+                    Users.Remove(user);
+                }
+            }
+        }
+
+        public string[] GetOnlineUsers()
+        {
+            return Users.OrderBy(x => x.Key).Select(x => x.Key).ToArray();
+        }
+    }
+}
